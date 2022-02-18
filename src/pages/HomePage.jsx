1dd621032791a64b2f02ba9f4199105e8593f01e
jonipark <park49j@mtholyoke.edu>
2022-02-18 18:20:20 -0500

    basic navbar/routes setting

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './HomePage.css'
+
+function HomePage() {
+
+    return (
+        <>
+            <div className='home-container'>
+                Home
+            </div>
+
+        </>
+    )
+}
+
+export default HomePage;
---
+++ src/pages/InitiativePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './InitiativePage.css'
+
+function InitiativePage() {
+
+    return (
+        <>
+            <div className='initiative-container'>
+                what we do
+            </div>
+
+        </>
+    )
+}
+
+export default InitiativePage;
