c5c1f5d356e3b4404b22657ec8b95abf86024d57
AdleAkbulut <akbul22a@mtholyoke.edu>
2022-03-04 16:42:18 -0500

    Adle's changes

---
+++ src/components/AboutPage/BoardInfo.js
@@ -0,0 +1,18 @@
+export const BoardInfo = [
+    {
+        team: "Website",
+        image: "hi",
+        name: "adle",
+        pronouns: 'she/her',
+        description: 'test',
+        chair: false,
+    },
+    {
+        team: "Website",
+        image: "hi",
+        name: "joni",
+        pronouns: 'she/her',
+        description: 'test',
+        chair: true,
+    },
+]
